292292fd9c0569b6a9d0fbd5ddffa9ef4618c2f6
Govind <govindsvyadav@gmail.com>
2023-05-08 11:06:15 +0530

    mobile-menu

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,48 @@
 import { Link } from "react-router-dom"
 import "../scss/navbar.scss"    
 import {BiMenuAltRight} from "react-icons/bi"  
+import { useState } from "react"
 
 export const Navbar = () => {
+
+  const [menu,setMenu] = useState(false) ;
+  const MenuHandler = ()=>{
+
+    setMenu(true);
+    if(menu){
+      setMenu(false);
+    }
+  }
   return (
    <div className="navbar">
           <div className="brand">
                 <h3><Link to={"/"}>Govind yadav</Link></h3>
           </div>
           <div className="menu">
-           <Link to={"/"} className="link">Home</Link>
+           <Link to={"/"}>Home</Link>
            <Link to={"/projects"}>Projects</Link>
            <Link to={"/blogs"}>Blogs</Link>
            <Link to={"/contact"}>Contact me</Link>
           </div>
-          <h3 className="mobile"><BiMenuAltRight /></h3>
+         <div className="mobile">
+         <h3><BiMenuAltRight onClick={MenuHandler} /></h3>
+           <MobileMenu menu={menu} setMenu={setMenu}/>
+         </div>
    </div>
+  )
+}
+
+const MobileMenu = ({menu,setMenu})=>{
+  return(
+    <>
+    {
+      menu ? (<div className="mobile-menu">
+      <Link to={"/"} onClick={()=>setMenu(false)}>Home</Link>
+      <Link to={"/projects"} onClick={()=>setMenu(false)}>Projects</Link>
+      <Link to={"/blogs"} onClick={()=>setMenu(false)}>Blogs</Link>
+      <Link to={"/contact"} onClick={()=>setMenu(false)}>Contact me</Link>
+     </div>) : null
+    }
+    </>
   )
 }
