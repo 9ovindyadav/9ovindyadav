432f91684439815b09299fc107c5eaca2d371dd2
Govind <govindsvyadav@gmail.com>
2023-05-19 19:05:52 +0530

    projects updated

--- src/assets/projects.jsx
+++ src/assets/projects.jsx
@@ -1,8 +1,28 @@
 export const projects = [
-    {
+  {
+    title : "Manage Personal Jobs API",
+    repo_url : "https://github.com/9ovindyadav/personal-jobs",
+    live_url : "https://personal-jobs.onrender.com/"
+  },
+  {
+    title : "JWT token",
+    repo_url : "https://github.com/9ovindyadav/jwt-tokens",
+    live_url : "https://jwt-token-ud20.onrender.com/"
+  },
+  {
+    title : "Store API",
+    repo_url : "https://github.com/9ovindyadav/store-api",
+    live_url : "https://store-api-aukd.onrender.com/"
+  },
+  {
+    title : "Task-manager(Data stored Online)",
+    repo_url : "https://github.com/9ovindyadav/task-manager",
+    live_url : "https://task-manager-77t6.onrender.com/"
+  },    
+  {
         title : "Calculator",
         repo_url : "https://github.com/9ovindyadav/calculator",
-        live_url : "https://calculator-pi-five.vercel.app"
+        live_url : "https://calculator-g.vercel.app"
       },
       {
         title : "World Countries",
@@ -25,7 +45,7 @@ export const projects = [
         live_url : "https://paragraph-generator-nine.vercel.app/"
       },
       {
-        title : "Task manager",
+        title : "Task manager (Offline-data)",
         repo_url : "https://github.com/9ovindyadav/Task_Manager",
         live_url : "https://task-manager-govind312.vercel.app"
       },
